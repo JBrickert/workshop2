bf762862fe82d2b2c7ebdf0cfe98322860e76f14
Jon Brickert <jbrickert1207@gmail.com>
2024-08-27 20:03:37 -0700

    app/nc_tutorials/settings.py updated

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'nc_tutorials.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('nc_tutorials_db'),
         'USER': config('postgres'),
         'PASSWORD': config('admin123'),
