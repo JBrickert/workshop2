316788154cd4c230b64c23e004a6fa8dd639776a
Jon Brickert <jbrickert1207@gmail.com>
2024-08-27 19:32:18 -0700

    updated app/nc_tutorials/settings.py

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'nc_tutorials.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'NAME': config('nc_tutorials_db'),
+        'USER': config('postgres'),
+        'PASSWORD': config('admin123'),
+        'HOST': config('nctutorials.c54qo2aig3ly.us-west-2.rds.amazonaws.com'),
+        'PORT': config('5432'),
     }
 }
 
